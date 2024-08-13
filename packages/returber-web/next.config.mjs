75597e6c807ebe7ba4ff5e646e74be2661db8349
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-08-13 17:22:35 +0300

    returber-web > setup: package

--- packages/returber-web/next.config.mjs
+++ packages/returber-web/next.config.mjs
@@ -1,9 +1,54 @@
 /** @type {import('next').NextConfig} */
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+
+import BundleAnalyzer from '@next/bundle-analyzer';
+import NextPWA from 'next-pwa';
+
+
+
+const withBundleAnalyzer = BundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+const withPWA = NextPWA({
+    dest: 'public',
+    maximumFileSizeToCacheInBytes: 50000000,
+});
+
+
 const nextConfig = {
-    output: 'export',
     images: {
         unoptimized: true,
     },
+    reactStrictMode: true,
+    webpack: (config, { }) => {
+        config.resolve.extensions.push('.ts', '.tsx');
+        config.resolve.fallback = { fs: false };
+
+        config.plugins.push(
+            new NodePolyfillPlugin(),
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: './node_modules/onnxruntime-web/dist/ort-wasm.wasm',
+                        to: 'static/chunks/',
+                    }, {
+                        from: './node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
+                        to: 'static/chunks/',
+                    },
+                    {
+                        from: './models',
+                        to: 'static/chunks/',
+                    },
+                ],
+            }),
+        );
+
+        return config;
+    },
+
 };
 
-export default nextConfig;
+
+export default withBundleAnalyzer(withPWA(nextConfig));
