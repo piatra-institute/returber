40258291e1e4091d9b6b4332a8c9d2db1ce8e3a2
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-08-15 17:47:18 +0300

    returber-api > dev: services/geocoder

--- packages/returber-api/source/services/geocoder.ts
+++ packages/returber-api/source/services/geocoder.ts
@@ -4,6 +4,28 @@ import reverseGeocoder, {
 
 
 
+export const countryCodes = [
+    'GB', // United Kingdom (English)
+    'US', // United States (English)
+    'DK', // Denmark (Danish)
+    'DE', // Germany (German)
+    'EE', // Estonia (Estonian)
+    'FI', // Finland (Finnish)
+    'IL', // Israel (Hebrew)
+    'HR', // Croatia (Croatian)
+    'IS', // Iceland (Icelandic)
+    'KR', // South Korea (Korean)
+    'LT', // Lithuania (Lithuanian)
+    'LV', // Latvia (Latvian)
+    'NL', // Netherlands (Dutch)
+    'NO', // Norway (Norwegian)
+    'PL', // Poland (Polish)
+    'RO', // Romania (Romanian)
+    'SK', // Slovakia (Slovak)
+    'SE', // Sweden (Swedish)
+];
+
+
 export const getReverseGeocode = async (location: any) => {
     const data = await new Promise<AddressObject>(
         (resolve, reject) => {
@@ -27,7 +49,7 @@ export const getReverseGeocode = async (location: any) => {
 
 export const initializeGeocoder = async () => {
     reverseGeocoder.init({
-        // countries: ['GB'],
+        countries: countryCodes,
     }, async function () {
         // geocoder is loaded and ready to run
         await getReverseGeocode({ latitude: 51.4934, longitude: 0.0098 });
