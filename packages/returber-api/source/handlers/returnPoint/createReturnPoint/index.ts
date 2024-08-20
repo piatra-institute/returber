6ed6b6c078dc1e732151b1868a10020922e8595a
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-08-20 12:09:55 +0300

    returber-api > dev: data, returnPoint

--- packages/returber-api/source/handlers/returnPoint/createReturnPoint/index.ts
+++ packages/returber-api/source/handlers/returnPoint/createReturnPoint/index.ts
@@ -3,13 +3,21 @@ import type {
     Response,
 } from 'express';
 
+import { v4 as uuid } from 'uuid';
+
 import { sql } from 'drizzle-orm';
 
-import database from '../../../database';
+import database from '@/source/database';
+import {
+    returnPoints,
+} from '@/source/database/schema/returnPoints';
+import {
+    returnPointLocationIndex,
+} from '@/source/database/schema/returnPointLocations';
 
 import {
     logger,
-} from '../../../utilities';
+} from '@/source/utilities';
 
 
 
@@ -19,12 +27,39 @@ export default async function handler(
 ) {
     try {
         const {
-
+            name,
+            image,
+            location,
         } = request.body;
 
         const createdAt = new Date().toISOString();
         const createdBy = 'system';
 
+
+        const returnPointIndexResult = await database.insert(returnPointLocationIndex).values({
+            minX: location.longitude,
+            maxX: location.longitude,
+            minY: location.latitude,
+            maxY: location.latitude,
+        });
+        const locationIndexID = Number(returnPointIndexResult.lastInsertRowid);
+        if (!locationIndexID) {
+            response.status(500).json({
+                status: false,
+            });
+            return;
+        }
+
+        await database.insert(returnPoints).values({
+            id: uuid(),
+            createdBy,
+            createdAt,
+            name: '',
+            image: '',
+            locationIndexID,
+            status: 'active',
+            queue: 0,
+        });
 
 
         response.json({
