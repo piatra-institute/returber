aadf5610de3be671c36e2394a07d0dbed2c0a61b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-08-28 13:01:23 +0300

    returber-web > dev: components/ReturberHome

--- packages/returber-web/components/ReturberHome/index.tsx
+++ packages/returber-web/components/ReturberHome/index.tsx
@@ -14,12 +14,14 @@ export default function ReturberHome({
             className={
                 `${absolute ? 'absolute top-0 left-1/2 transform -translate-x-1/2' : ''} flex justify-center items-center`
             }
+            draggable={false}
         >
             <Image
                 src="/returber-logo.png"
                 alt="Returber Logo"
                 width={200} height={200}
                 priority={true}
+                draggable={false}
                 className="pointer-events-none select-none m-8 w-[100px] h-[100px] lg:w-[200px] lg:h-[200px]"
             />
         </Link>
